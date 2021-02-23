507c04583aa6237abc0694a8d40a273273c4f480
Sugarcothe <63057848+Sugarcothe@users.noreply.github.com>
2021-02-23 19:48:33 +0100

    How to fetch news data and render it using react hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React, { Component, useState, useEffect} from 'react';
 
-const App =() => {
-  const [count, setCount] = useState(0);
+
+const App = () => {
+
+  const [news, setNews] = useState({})
+  // fetch news
+  const fetchNews = () => {
+    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    .then(result => result.json())
+    // .then(data => console.log(data))
+    .then(data => setNews(data.hits))
+    .catch(error => console.log(error))
+  };
 
   useEffect(() => {
-    document.title = `Clicked ${count} times`
-  })
+    fetchNews();
+  });
 
-  const increment = () => setCount(count + 1)
-  
   return (
-    <div className="App">
-      <h2>Counter App</h2>
-      <button onClick={increment} className="btn-success btn-sm">Clicked {count} times</button>
+    <div>
+      <h2>News</h2>
+        {news.map((news, index) => (
+        <p keys={index}>{news.title}</p>
+        ))}
     </div>
-
-  );
+  )
 }
+
+// const App =() => {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     document.title = `Clicked ${count} times`
+//   })
+
+//   const increment = () => setCount(count + 1)
+  
+//   return (
+//     <div className="App">
+//       <h2>Counter App</h2>
+//       <button onClick={increment} className="btn-success btn-sm">Clicked {count} times</button>
+//     </div>
+
+//   );
+// }
 
 
 // class App extends Component {
